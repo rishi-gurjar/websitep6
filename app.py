85cdd1723e26443cabbd2bcbdb15ff2cbae18938
rishi-gurjar <rishigurjar@icloud.com>
2021-12-19 13:38:44 -0800

    flask db

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+import yaml
 import sqlite3
 
 app = Flask(__name__)
 
+#configure db
+db = yaml.safe_load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+mysql = MySQL(app) 
 
 @app.route('/')
 def home():
@@ -16,8 +26,17 @@ def top():
 def about():
     return render_template("about.html")
 
-@app.route('/stats')
+@app.route('/stats', methods = ['GET', 'POST'])
 def stats():
+    if request.method == 'POST':
+        userDetails = request.form
+        name = userDetails['name']
+        email = userDetails['email']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email) VALUES(%s, %s)", (name, email))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
     return render_template("stats.html")
 
 @app.route('/sports')
@@ -30,6 +49,12 @@ if __name__ == "__main__":
 
 # pip install Flask
 # py app.py
+# OR FLASK_APP=app.py flask run
+
+# export FLASK_APP=app  
+# flask run
+
+# python3 app.py
  # https://www.educative.io/edpresso/how-to-add-data-to-databases-in-flask
  # "database search flask"
  # https://www.w3schools.com/css/tryit.asp?filename=trycss_forms
